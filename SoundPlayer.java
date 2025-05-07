d7a1700d7bac5755efc5dd6f3da12ba2b616415c
David <dzw5600@psu.edu>
2025-05-07 17:25:57 -0400

    Making sound quieter

--- SoundPlayer.java
+++ SoundPlayer.java
@@ -11,6 +11,12 @@ public class SoundPlayer {
                     AudioInputStream audioInput = AudioSystem.getAudioInputStream(soundFile);
                     Clip clip = AudioSystem.getClip();
                     clip.open(audioInput);
+
+                    FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                    float range = gainControl.getMaximum() - gainControl.getMinimum();
+                    float gain = (range * 0.7f) + gainControl.getMinimum();
+                    gainControl.setValue(gain);
+
                     clip.start();
                 } else {
                     System.out.println("Sound file not found: " + soundFileName);
